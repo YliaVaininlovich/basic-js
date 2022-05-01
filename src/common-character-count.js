e01d4075849ff15b2a14cff0a594f3934f24a9c9
YliaVaininlovich <Ylia.v@tut.by>
2022-05-01 23:48:42 +0300

    feat: common-character-count, vigenere-cipher

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -10,12 +10,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
+ * npm test --g test/common-character-count.test.js
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  const arr1 = s1.split("").sort();
+  const arr2 = s2.split("").sort();
+  const result = [];
+  let i = 0;
+  let j = 0;
+  while (i < arr1.length) {
+    if (arr1[i] === arr2[j]) {
+      result.push(arr1[i]);
+      i += 1;
+      j += 1;
+    } else if (arr1[i] < arr2[j]) {
+      i += 1;
+    } else {
+      j += 1;
+    }
+  }
+  return result.length;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,90 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ * npm test --g test/vigenere-cipher.test.js
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type) {
+    this.type = type;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    while (key.length < message.length) {
+      key += key;
+    }
+    let text = message.toUpperCase();
+    let keyArr = key.toUpperCase().split("");
+    let arr = [];
+    let codeA = "A".charCodeAt(0);
+    for (let i = 0; i < text.length; i += 1) {
+      if (text.charCodeAt(i) > 64 && text.charCodeAt(i) < 91) {
+        const current = text.charCodeAt(i) - codeA;
+        const shift = keyArr[0].charCodeAt(0) - codeA;
+        keyArr.shift();
+        const el = String.fromCharCode(codeA + ((current + shift) % 26));
+        arr.push(el);
+      } else {
+        arr.push(text[i]);
+      }
+    }
+
+    if (this.type === false) {
+      return arr.reverse().join("");
+    }
+    return arr.join("");
+  }
+
+  decrypt(message, key) {
+    if (!message || !key) {
+      throw new Error("Incorrect arguments!");
+    }
+
+    while (key.length < message.length) {
+      key += key;
+    }
+    let text = message.toUpperCase();
+    let keyArr = key.toUpperCase().split("");
+    let arr = [];
+    let codeA = "A".charCodeAt(0);
+    for (let i = 0; i < text.length; i += 1) {
+      if (text.charCodeAt(i) > 64 && text.charCodeAt(i) < 91) {
+        const current = text.charCodeAt(i) - codeA;
+        const shift = keyArr[0].charCodeAt(0) - codeA;
+        keyArr.shift();
+        const el = String.fromCharCode(codeA + ((current - shift + 26) % 26));
+        arr.push(el);
+      } else {
+        arr.push(text[i]);
+      }
+    }
+
+    if (this.type === false) {
+      return arr.reverse().join("");
+    }
+    return arr.join("");
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
